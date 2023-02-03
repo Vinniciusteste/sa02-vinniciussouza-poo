5a3acf1f1855ee533986dd646338f357f39e0233
Vinniciusteste <contateste.vinnicius@gmail.com>
2023-02-03 11:26:45 -0300

    entregando para o professor o melhor do mundo

---
+++ Sa02Poo/Sa02Poo.Aplicativo7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Sa02Poo.RegraDeNegocio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+
+namespace Sa02Poo.Aplicativo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal p1, p2, p3 ,p4 , p5;
+            Console.WriteLine("Qual o valor da peça 1?");
+            p1 = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a o calor da peça 2?");
+            p2 = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor da peça 3?");
+            p3 = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor da peça 4?");
+            p4 = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor da peça 5?");
+            p5 = Decimal.Parse(Console.ReadLine());
+
+            Aplicativo7RN app7 = new Aplicativo7RN();
+            Console.WriteLine("Valor total das peças: R$");
+            Console.WriteLine(app7.SomaTotalDePeças(p1, p2, p3, p4, p5));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Sa02Poo/Sa02Poo.App2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sa02Poo.RegraDeNegocio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02Poo.App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoinicial, anoatual;
+
+            anoinicial = int.Parse(Console.ReadLine());
+            anoatual = int.Parse(Console.ReadLine());
+            Aplicativo2RN app2 = new Aplicativo2RN();
+            Console.WriteLine(app2.SubtraçãoDoisAnos(anoinicial, anoatual));
+            Console.ReadKey();
+    
+        }
+    }
+}
